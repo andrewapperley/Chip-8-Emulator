28a61b5f5e8609d1d026dc798ee6894b91cf2159
Andrew Apperley <mail@andrewapperley.ca>
2015-03-15 18:24:03 -0400

    Initial Commit

---
+++ Chip-8 Emulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chip-8 Emulator
+//
+//  Created by Andrew Apperley on 2015-03-15.
+//  Copyright (c) 2015 Andrew Apperley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
